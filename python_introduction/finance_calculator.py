9ddad9c9c4b44ef4a497b9fcb10de7d0a517f5dc
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-05-22 16:01:43 +0200

    Cast input values to float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_income = int(input("Enter your monthly income: "))
-monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 savings = monthly_income - monthly_expenses
 projected_savings = savings * 12 + savings * 12 * 0.05
 
