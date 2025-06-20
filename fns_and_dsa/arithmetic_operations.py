a1fe74e0292759105c0fa5022c85f1cd5808bc67
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-06-20 21:24:24 +0200

    Change conditionals from Match-case to if-else

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,17 @@
 import math
 
 def perform_operation(num1, num2, operation):
-	match(operation):
-		case "add":
-			return num1 + num2
-		case "subtract":
-			return num1 - num2
-		case "multiply":
-			return num1 * num2
-		case "divide": 
-			if num2 == 0:
-				return "Division by zero"
-			else:
-				return num1 / num2
-		case _:
-			return "Unknown operation"
+
+	if operation == "add":
+		return num1 + num2
+	elif operation == "subtract":
+		return num1 - num2
+	elif operation == "multiply":
+		return num1 * num2
+	elif operation == "divide":
+		if num2 == 0:
+			return f"Division by zero"
+		else:
+			return num1 / num2
+	else:
+		return f"Unknown operation"
