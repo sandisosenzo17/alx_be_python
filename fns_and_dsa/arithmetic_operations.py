8df5cb20878ac1c38a241f5932e08444bb2e2b44
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-06-20 21:11:09 +0200

    Fix arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,24 +1,17 @@
 import math
 
 def perform_operation(num1, num2, operation):
-	
-	if math.isnan(num1):
-		return "Please provide numbers."
-	if math.isnan(num2):
-		return "Please provide numbers."
 	match(operation):
 		case "add":
-			return float(num1)+num2
+			return num1 + num2
 		case "subtract":
-			return float(num1)-num2
+			return num1 - num2
 		case "multiply":
-			return float(num1)*num2
-		case "divide":
+			return num1 * num2
+		case "divide": 
 			if num2 == 0:
 				return "Division by zero"
 			else:
-				return float(num1)/num2
+				return num1 / num2
 		case _:
 			return "Unknown operation"
-
-print("Test.")
