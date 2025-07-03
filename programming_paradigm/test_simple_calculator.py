b8e60e67f6e7f55f7e4988da22631d0694761270
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-07-03 19:13:07 +0200

    Rename object

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,29 +3,29 @@ from simple_calculator import SimpleCalculator
 
 class TestCalculator(unittest.TestCase):
   def setUp(self):
-    self.sc = SimpleCalculator()
+    self.calc = SimpleCalculator()
   
   def test_addition(self):
-    self.assertEqual(self.sc.add(4, 7), 11)
-    self.assertEqual(self.sc.add(-4, 7), 3)
-    self.assertEqual(self.sc.add(4, -7), -3)
-    self.assertEqual(self.sc.add(-4, -7), -11)
+    self.assertEqual(self.calc.add(4, 7), 11)
+    self.assertEqual(self.calc.add(-4, 7), 3)
+    self.assertEqual(self.calc.add(4, -7), -3)
+    self.assertEqual(self.calc.add(-4, -7), -11)
   
   def test_subtraction(self):
-    self.assertEqual(self.sc.subtract(2, 9), 11)
-    self.assertEqual(self.sc.subtract(-2, 9), 7)
-    self.assertEqual(self.sc.subtract(2, -9), -5)
-    self.assertEqual(self.sc.subtract(-2, -9), -11)
+    self.assertEqual(self.calc.subtract(2, 9), 11)
+    self.assertEqual(self.calc.subtract(-2, 9), 7)
+    self.assertEqual(self.calc.subtract(2, -9), -5)
+    self.assertEqual(self.calc.subtract(-2, -9), -11)
   
   def test_multiply(self):
-    self.assertEqual(self.sc.multiply(3, 6), 18)
-    self.assertEqual(self.sc.multiply(-3, 6), -18)
-    self.assertEqual(self.sc.multiply(3, -6), -18)
-    self.assertEqual(self.sc.multiply(-3, -6), 18)
+    self.assertEqual(self.calc.multiply(3, 6), 18)
+    self.assertEqual(self.calc.multiply(-3, 6), -18)
+    self.assertEqual(self.calc.multiply(3, -6), -18)
+    self.assertEqual(self.calc.multiply(-3, -6), 18)
   
   def test_divide(self):
-    self.assertEqual(self.sc.divide(18, 3), 6)
-    self.assertEqual(self.sc.divide(18, -3), -6)
-    self.assertEqual(self.sc.divide(18, "one"), "Invalid value")
-    self.assertEqual(self.sc.divide(4, 0), None)
+    self.assertEqual(self.calc.divide(18, 3), 6)
+    self.assertEqual(self.calc.divide(18, -3), -6)
+    self.assertEqual(self.calc.divide(18, "one"), "Invalid value")
+    self.assertEqual(self.calc.divide(4, 0), None)
 
