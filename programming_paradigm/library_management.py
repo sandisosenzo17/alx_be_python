97e536efc350ef82bc8578358847582e0f4105bf
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-07-04 12:01:08 +0200

    Library management app to add, checkout and return books

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,58 @@
+class Book:
+  def __init__(self, title, author):
+    self.title = title
+    self.author = author
+    self._is_checked_out = None
+  
+
+class Library:
+  def __init__(self):
+    self._books = []
+  
+  def add_book(self, title, author):
+    book = Book(title, author)
+    book._is_checked_out = 'n'
+    self._books.append(book)
+
+  def check_out_book(self, title):
+    for book in self._books:
+      if book.title == title:
+        book._is_checked_out = 'y'
+
+  def return_book(self, title):
+    for book in self._books:
+      if book.title == title:
+        book._is_checked_out = 'n'
+  
+  def list_available_books(self):
+    for book in self._books:
+      if book._is_checked_out == 'y':
+        continue
+      else:
+        print(f"{book.title} by {book.author}")
+
+
+def main():
+  library = Library()
+  library.add_book("Brave New World", "Aldous Huxley")
+  library.add_book("1984", "George Orwell")
+  library.add_book("Physics of the impossible", "Michio Kaku")
+
+  print("Available books after setup:")
+  library.list_available_books()
+
+  library.add_book("LOTR", "Peter Jackson")
+  print("\nAvailable books after addition:")
+  library.list_available_books()
+
+  library.check_out_book("1984")
+  print("\nAvailable books after checking out '1984':")
+  library.list_available_books()
+
+  library.return_book("1984")
+  print("\nAvailable books after returning '1984':")
+  library.list_available_books()
+
+
+if __name__ == "__main__":
+  main()
