81a3785664c8ce982f949d0f4c785e98998360f7
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-07-07 01:19:03 +0200

    Demonstrate knowledge of magic methods

---
+++ oop/book_class.py
@@ -0,0 +1,26 @@
+class Book:
+  def __init__(self, title, author, year):
+    self.title = title
+    self.author = author
+    self.year = year
+
+  def __del__(self):
+    print(f"Deleting {self.title}")
+
+  def __str__(self):
+    return f"{self.title} by {self.author}, published in {self.year}"
+  
+  def __repr__(self):
+    return f"Book({self.title}, {self.author}, {self.author})"
+  
+
+def main():
+  book = Book("1984", "George Orwell", 1949)
+
+  print(book)
+  print(repr(book))
+
+  del book
+
+if __name__ == "__main__":
+  main()
